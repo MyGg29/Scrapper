857ea75adeee8cc908db0c37cf932ef4ad256bb7
rodolpheh <houdas.rodolphe@gmail.com>
2017-10-09 22:45:52 +0200

    Removed icalendar dependency and cleaning code

--- PlanningAP3.py
+++ PlanningAP3.py
@@ -1,7 +1,6 @@
-import requests, sys, re, locale
+import requests, re, locale
 from bs4 import BeautifulSoup
 from datetime import datetime
-from icalendar import Calendar, Event, vCalAddress, vText
 
 url = "https://aurion-lille.isen.fr/faces/Planning.xhtml"
 events = []
@@ -61,37 +60,21 @@ with requests.Session() as s:
         if DOMInput.get_text() == "Au":
             classEndHour = DOMInput.parent.parent.parent.contents[7].get_text()
         if DOMInput.get_text() == "Matière":
-            classLesson = DOMInput.parent.parent.parent.contents[3].get_text()
-            eventsData.append({"startingTime": datetime.strptime(" ".join(classDate.split(" ")[1:]) + " " + classStartHour, "%d %B %Y %H:%M"),
-                               "stoppingTime": datetime.strptime(" ".join(classDate.split(" ")[1:]) + " " + classEndHour, "%d %B %Y %H:%M"),
-                               "title": classLesson})
+            eventsData.append({"startingTime": datetime.strptime(classDate + " " + classStartHour, "%A %d %B %Y %H:%M"),
+                               "stoppingTime": datetime.strptime(classDate + " " + classEndHour, "%A %d %B %Y %H:%M"),
+                               "title": DOMInput.parent.parent.parent.contents[3].get_text()})
 
     for DOMInput in parser.find_all("tr", id = re.compile(".*j_idt212:0")):
-        classTeacherName = DOMInput.contents[0].get_text()
-        classTeacherFirstname = DOMInput.contents[1].get_text()
-        eventsTeacher.append({"teacher": classTeacherFirstname + " " + classTeacherName})
+        eventsTeacher.append({"teacher": DOMInput.contents[1].get_text() + " " + DOMInput.contents[0].get_text()})
 
     for DOMInput in parser.find_all("td", id = re.compile(".*0:j_idt205")):
         eventsLocation.append({"location": DOMInput.get_text()})
-        print(DOMInput.get_text())
-
-    cal = Calendar()
 
     icalString += "BEGIN:VCALENDAR\r\n"
 
     for i in range(0, len(eventsData)):
         icalString += "BEGIN:VEVENT\r\n"
         events.append({**eventsData[i], **eventsTeacher[i], **eventsLocation[i]})
-        calEvent = Event()
-        calEvent['uid'] = i
-        calEvent['dtstart'] = datetime.strftime(events[i]["startingTime"], "%Y%m%dT%H%M%S")
-        calEvent['dtstamp'] = datetime.strftime(events[i]["startingTime"], "%Y%m%dT%H%M%S")
-        calEvent['dtend'] = datetime.strftime(events[i]["stoppingTime"], "%Y%m%dT%H%M%S")
-        calEvent['summary'] = events[i]["title"]
-        calEvent['organizer'] = vCalAddress(events[i]["teacher"])
-        calEvent['description'] = vCalAddress(events[i]["teacher"])
-        calEvent['location'] = events[i]["location"]
-        cal.add_component(calEvent)
         icalString += "UID:" + str(i) + "\r\n"
         icalString += "DTSTART:" + datetime.strftime(events[i]["startingTime"], "%Y%m%dT%H%M%S") + "\r\n"
         icalString += "DTEND:" + datetime.strftime(events[i]["stoppingTime"], "%Y%m%dT%H%M%S") + "\r\n"
@@ -103,10 +86,5 @@ with requests.Session() as s:
 
     icalString += "END:VCALENDAR\r\n"
 
-    with open("calendar.ics", "wb") as f:
-        f.write(cal.to_ical())
-
-    with open("calendar2.ics", "w") as f:
+    with open("calendar.ics", "w") as f:
         f.write(icalString)
-    
-    
