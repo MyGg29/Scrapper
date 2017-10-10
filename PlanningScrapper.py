7be3226b1bd5b0624b1a09c8e6f47bc7775f25b7
rodolpheh <houdas.rodolphe@gmail.com>
2017-10-10 15:37:59 +0200

    Added comments, changed arguments and parsing system, updated README

--- PlanningScrapper.py
+++ PlanningScrapper.py
@@ -1,37 +1,30 @@
 import requests, re, locale, sys
 from bs4 import BeautifulSoup
 from datetime import datetime, date, timedelta
+import argparse
 
-if len(sys.argv) <= 1:
-	print("Usage: python3 PlanningScrapper.py <year> [start date] [end date]")
-	print("Example: python3 PlanningScrapper.py AP3 09/10/2017 29/10/2017")
-	exit()
-else:
-	if sys.argv[1] in ["AP3", "AP4", "AP5", "M1", "M2", "CPG1", "CPG2", "CSI3", "CSIU3", "CIR1", "CIR2", "CIR3"]:
-		studentGroup = sys.argv[1]
-	else:
-		print("The year is not valid")
-		exit()
-if len(sys.argv) >= 3:
-	startDate = sys.argv[2]
-else:
-	startDate = datetime.strftime(date.today(), "%d/%m/%Y")
-if len(sys.argv) >= 4:
-	endDate = sys.argv[3]
-else:
-	endDate = datetime.strftime(date.today() + timedelta(days=6), "%d/%m/%Y")
+## Setup the argument parser and parse them
+argsParser = argparse.ArgumentParser(description = "Scraps ISEN's planning website. Outputs an .ics file in the current directory.", prog = "python3 PlanningScrapper.py")
+argsParser.add_argument("-g", help = "Set the group", required = True, metavar = "<group>", dest = "studentGroup")
+argsParser.add_argument("-s", help = "Start day", metavar = "<start date>", default = datetime.strftime(date.today(), "%d/%m/%Y"), dest = "startDate")
+argsParser.add_argument("-e", help = "End day", metavar = "<end date>", default = datetime.strftime(date.today() + timedelta(days=6), "%d/%m/%Y"), dest = "endDate")
+argsParser.add_argument("-o", help = "Name for the outputted file, without the extension", metavar = "<filename>", required = True, dest = "outputFile")
+args = argsParser.parse_args()
 
-print("Start date: " + startDate)
-print("End date: " + endDate)
+## Display the dates
+print("Start date: " + args.startDate)
+print("End date: " + args.endDate)
 
 url = "https://aurion-lille.isen.fr/faces/Planning.xhtml"
-events = []
 eventsData = []
 eventsTeacher = []
 eventsLocation = []
 
 icalString = ""
 
+## Set the locale depending on the system
+## (Windows is using a different locale string)
+## We set it in French because the months and weekdays are in French
 if sys.platform in ['win32']:
 	locale.setlocale(locale.LC_ALL, 'fra')
 	print("Locale set for Windows platform")
@@ -39,6 +32,7 @@ else:
 	locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
 	print("Locale set for Unix platform")
 
+## Open a session and start retrieving data
 with requests.Session() as s:
 	r = s.get(url)
 	print("Response URL: " + r.url)
@@ -48,22 +42,26 @@ with requests.Session() as s:
 	payload = {"form": "form",
 			   "form:largeurDivCenter": "1142"}
 
+	## The website use Richfaces forms for navigation,
+	## We first need to find the j_idt code for the button we want to press
 	for DOMInput in parser.find_all("span", "rf-ddm-itm-lbl"):
-		if studentGroup in DOMInput.get_text():
+		if args.studentGroup in DOMInput.get_text():
 			payload[DOMInput.parent["id"]] = DOMInput.parent["id"]
+	## Then we need something called "javax.faces.ViewState", whatever it is
 	payload["javax.faces.ViewState"] = parser.select_one("#j_id1:javax.faces.ViewState:0")["value"]
 	print("Payload: " + str(payload))
 	r = s.post(url, params = payload)
+	## By now, we should have received https://aurion-lille.isen.fr/faces/ChoixPlanning.xhtml
 	print("Response URL: " + r.url)
 	parser = BeautifulSoup(r.text.encode(encoding='UTF-8',errors='strict'), 'html.parser')
 	print("Cookies: " + str(s.cookies))
 	payload = {"form": "form",
 			   "form:largeurDivCenter": "1154",
 			   "form:j_idt263-value": "false",
-			   "form:calendarDebutInputDate": startDate,
-			   "form:calendarDebutInputCurrentDate": "/".join(startDate.split("/")[1:]),
-			   "form:calendarFinInputDate": endDate,
-			   "form:calendarFinInputCurrentDate": "/".join(endDate.split("/")[1:]),
+			   "form:calendarDebutInputDate": args.startDate,
+			   "form:calendarDebutInputCurrentDate": "/".join(args.startDate.split("/")[1:]),
+			   "form:calendarFinInputDate": args.endDate,
+			   "form:calendarFinInputCurrentDate": "/".join(args.endDate.split("/")[1:]),
 			   "form:dataTableFavori:j_idt150": "on",
 			   "form:dataTableFavori:0:j_idt151": "on",
 			   "javax.faces.source": "form:j_idt110",
@@ -79,39 +77,57 @@ with requests.Session() as s:
 	print("Payload: " + str(payload))
 	r = s.post(r.url, params = payload)
 
+	## Now for some parsing of the page
 	parser = BeautifulSoup(r.text.encode(encoding='UTF-8',errors='strict'), 'html.parser')
+
+	## We build the list of events by finding all labels like "Du", "Au" and
+	## "Matière" and getting the next sibling label which hold the information
+	## we want (starting time, ending time and subject)
 	for DOMInput in parser.find_all("span", "ev_libelle"):
+		## The sibling of "Du" holds the starting time
 		if DOMInput.get_text() == "Du":
 			classDate = DOMInput.parent.parent.parent.contents[3].get_text()
 			classStartHour = DOMInput.parent.parent.parent.contents[7].get_text()
+		## The sibling of "Au" holds the ending time
 		if DOMInput.get_text() == "Au":
 			classEndHour = DOMInput.parent.parent.parent.contents[7].get_text()
+		## The sibling of "Matière" holds the class's subject
 		if DOMInput.get_text() == "Matière":
 			eventsData.append({"startingTime": datetime.strptime(classDate + " " + classStartHour, "%A %d %B %Y %H:%M"),
 							   "stoppingTime": datetime.strptime(classDate + " " + classEndHour, "%A %d %B %Y %H:%M"),
 							   "title": DOMInput.parent.parent.parent.contents[3].get_text()})
 
+	## We build the list of teachers. If nothing goes wrong, it should be
+	## built in the same order than the events, thus having each element in the
+	## events list correspond to an element in the teachers list with the same
+	## index
 	for DOMInput in parser.find_all("tr", id = re.compile(".*j_idt212:0")):
 		eventsTeacher.append({"teacher": DOMInput.contents[1].get_text() + " " + DOMInput.contents[0].get_text()})
 
+	## We build the list of locations. If nothing goes wrong, it should be
+	## built in the same order than the events, thus having each element in the
+	## events list correspond to an element in the locations list with the same
+	## index
 	for DOMInput in parser.find_all("td", id = re.compile(".*0:j_idt205")):
 		eventsLocation.append({"location": DOMInput.get_text()})
 
+	## Start creating the icalendar-compatible file
 	icalString += "BEGIN:VCALENDAR\r\n"
 
+	## Create all the events in the VEVENT format
 	for i in range(0, len(eventsData)):
 		icalString += "BEGIN:VEVENT\r\n"
-		events.append({**eventsData[i], **eventsTeacher[i], **eventsLocation[i]})
 		icalString += "UID:" + str(i) + "\r\n"
-		icalString += "DTSTART:" + datetime.strftime(events[i]["startingTime"], "%Y%m%dT%H%M%S") + "\r\n"
-		icalString += "DTEND:" + datetime.strftime(events[i]["stoppingTime"], "%Y%m%dT%H%M%S") + "\r\n"
-		icalString += "SUMMARY:" + events[i]["title"] + "\r\n"
-		icalString += "ORGANIZER:" + events[i]["teacher"] + "\r\n"
-		icalString += "DESCRIPTION:" + events[i]["teacher"] + "\r\n"
-		icalString += "LOCATION:" + events[i]["location"] + "\r\n"
+		icalString += "DTSTART:" + datetime.strftime(eventsData[i]["startingTime"], "%Y%m%dT%H%M%S") + "\r\n"
+		icalString += "DTEND:" + datetime.strftime(eventsData[i]["stoppingTime"], "%Y%m%dT%H%M%S") + "\r\n"
+		icalString += "SUMMARY:" + eventsData[i]["title"] + "\r\n"
+		icalString += "ORGANIZER:" + eventsTeacher[i]["teacher"] + "\r\n"
+		icalString += "DESCRIPTION:" + eventsTeacher[i]["teacher"] + "\r\n"
+		icalString += "LOCATION:" + eventsLocation[i]["location"] + "\r\n"
 		icalString += "END:VEVENT\r\n"
 
 	icalString += "END:VCALENDAR\r\n"
 
-	with open("calendar.ics", "w") as f:
+	## Write to the output file
+	with open(args.outputFile + ".ics", "w") as f:
 		f.write(icalString)
--- README.md
+++ README.md
@@ -5,16 +5,26 @@ Pythons scripts to get the most out of ISEN's Aurion system.
 
 ### What is there so far ?
 
-**CASLogin.py** : by editing the script to enter your username and password, you can connect a python-request session to the authentication system for the school system. Not used yet but could be useful someday.
+#### CASLogin.py
+
+By editing the script to enter your username and password, you can connect a python-request session to the authentication system for the school system. Not used yet but could be useful someday.
 
 Dependencies: python-requests python-beautifulsoup4
 
-**PlanningScrapper.py** : scraps the planning into an .ics file
+#### PlanningScrapper.py
 
-Usage: `python3 PlanningScrapper.py <year> [start date] [end date]`
+```
+usage: python3 PlanningScrapper.py [-h] -g <group> [-s <start date>]
+                                   [-e <end date>] -o <filename>
 
-Example: `python3 PlanningScrapper.py AP3 09/10/2017 29/10/2017`
+Scraps ISEN's planning website. Outputs an .ics file in the current directory.
 
-Outputs an .ics file in the current directory. This file can then be imported in Thunderbird/Lightning
+optional arguments:
+  -h, --help       show this help message and exit
+  -g <group>       Set the group
+  -s <start date>  Start day
+  -e <end date>    End day
+  -o <filename>    Name for the outputted file, without the extension
+  ```
 
 Dependencies: python-requests python-beautifulsoup4
