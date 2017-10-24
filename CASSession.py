22812d4cc0162b98d2e5c5df3803efb28fe91495
rodolpheh <houdas.rodolphe@gmail.com>
2017-10-24 22:29:53 +0200

    Moving CASLogin.py to CASSession.py and turning it into a class

---
+++ CASSession.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+class CASSession:
+
+    url = "https://cas.isen.fr/login"
+    password = "YOURPASSWORDHERE"
+    username = "YOURUSERNAMEHERE"
+
+    def __init__(self):
+        print("Initiating CASSession object")
+
+    def getSession(self):
+        reqSession = requests.Session()
+        response = reqSession.get(self.url)
+
+        parser = BeautifulSoup(response.text.encode(encoding='UTF-8',errors='strict'), 'html.parser')
+        payload = {}
+
+        for DOMInput in parser.findAll(name = "input"):
+            if DOMInput['name'] == 'lt':
+                payload['lt'] = DOMInput['value']
+
+        payload['username'] = self.username
+        payload['password'] = self.password
+        response = reqSession.post(response.url, params = payload)
+        return reqSession
+
+    def setUsername(self, newUsername):
+        self.username = newUsername
+
+    def setPassword(self, newPassword):
+        self.password = newPassword
+
+    def __enter__(self):
+        self.session = self.getSession()
+        return self.session
+
+    def __exit__(self, type, value, traceback):
+        self.session.close()
